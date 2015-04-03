b5c37f8e77dce99dc55b2496c4ee9b57423d1177
Christopher Nellis <quizy101@gmail.com>
2015-04-03 00:06:43 -0400

    Updated Books Class, Added BookRatings Class

---
+++ classes/bookClass/BookRating.java
@@ -0,0 +1,117 @@
+package com.company;
+
+/**
+ * Created by Christopher on 4/2/2015.
+ */
+public class BookRating {
+
+    float fltRating; // Float number of a rating. 0.0-5.0 Range.
+    float fltLongRating; // Float long book rating. 0.0-5.0 Range. 0 for short, 5 for long.
+    float fltFicNonFicRating; // Float of fiction/non-fiction rating. -5.0-5.0 Range. -5 for fiction, 5 for Non-fiction.
+    float fltGenreRating; // Float of Genre Rating. 0.0-5.0 Range.
+    float[][] arrRating = new float[2][2];
+
+    BookRating(){
+        fltRating = 0.0f;
+        fltLongRating = 0.0f;
+        fltFicNonFicRating = 0.0f;
+        fltGenreRating = 0.0f;
+
+    }
+
+    BookRating(float pRating, float pLongRating, float FicNonFicRating, float GenreRating) {
+        fltRating = pRating;
+        fltLongRating = pLongRating;
+        fltFicNonFicRating = FicNonFicRating;
+        fltGenreRating = GenreRating;
+
+    }
+
+    public float getFltRating() {
+        return fltRating;
+    }
+
+    public void setFltRating(float fltRating) {
+        this.fltRating = fltRating;
+    }
+
+    public float getFltLongRating() {
+        return fltLongRating;
+    }
+
+    public void setFltLongRating(float fltLongRating) {
+        this.fltLongRating = fltLongRating;
+    }
+
+    public float getFltFicNonFicRating() {
+        return fltFicNonFicRating;
+    }
+
+    public void setFltFicNonFicRating(float fltFicNonFicRating) {
+        this.fltFicNonFicRating = fltFicNonFicRating;
+    }
+
+    public float getFltGenreRating() {
+        return fltGenreRating;
+    }
+
+    public void setFltGenreRating(float fltGenreRating) {
+        this.fltGenreRating = fltGenreRating;
+    }
+
+    public float[][] getArrRating() {
+        return arrRating;
+    }
+
+    public void setArrRating(int row, int col, float arrRating) {
+        this.arrRating[row][col] = arrRating;
+    }
+
+    public String toString(){
+        return this.getFltRating() + ", " + this.fltLongRating + ", " + this.getFltFicNonFicRating() + ", " + this.getFltGenreRating();
+    }
+
+    public String toString(Books pBook){
+        float[] rtnRatingsArr = this.getRatings(pBook);
+        return rtnRatingsArr[0] + ", " + rtnRatingsArr[1] + ", " + rtnRatingsArr[2] + ", " + rtnRatingsArr[3];
+    }
+
+    public void setRatings(Books pBook, float pRating, float pLongRating, float pFicNonFicRating, float pGenreRating) {
+        this.setFltRating(pRating);
+        this.setFltLongRating(pLongRating);
+        this.setFltFicNonFicRating(pFicNonFicRating);
+        this.setFltGenreRating(pGenreRating);
+        if (pBook.isBookLong(pBook)) {
+            this.setArrRating(1, 0, pLongRating);
+        } else {
+            this.setArrRating(0, 0, pLongRating);
+        }
+        if (pBook.getBoolFicNonFic()){
+            this.setArrRating(1, 1, pFicNonFicRating);
+        }
+        else {
+            this.setArrRating(0, 1, pFicNonFicRating);
+        }
+    }
+
+    public float[] getRatings(Books pBook){
+        float[] rtnArrRating = new float[4];
+        rtnArrRating[0] = this.getFltRating();
+        if (pBook.isBookLong(pBook)) {
+            rtnArrRating[1] = this.getArrRating()[1][0]-(this.getArrRating()[1][0]*2);
+        }
+        else {
+
+            rtnArrRating[1] = this.getArrRating()[0][0];
+        }
+        if (pBook.getBoolFicNonFic()){
+            rtnArrRating[2] = this.getArrRating()[1][1]-(this.getArrRating()[1][1]*2);
+        }
+        else {
+            rtnArrRating[2] = this.getArrRating()[0][1];
+        }
+        rtnArrRating[3] = this.getFltGenreRating();
+
+        return rtnArrRating;
+    }
+}
--- classes/bookClass/Books.java
+++ classes/bookClass/Books.java
@@ -1,114 +1,172 @@
+package com.company;
+
 /* Class: Books
 *  Author: Christopher Nellis
 *  Description: Stores information about the Books class.
-*               
+*
 */
 
-public static class Books {
+public class Books {
+    /* Private ID counter will increment after every new book. */
+    static int intIDCounter = 0;
 
     /* Properties */
-    long lngISBN; // Instead of ID, I propose using the ISBN. They should be unique.
+    int intID; // Book identifier.
+    long lngISBN; // Long int of ISBN. They should be unique.
     String strTitle; // Book title.
     String strAuthor; // Book Author.
     String strPublisher; // Book Publisher? Is this needed?
-    int intPubDate; // Publication Date
+    boolean boolFicNonFic; // Boolean fiction or non-fiction. Fiction=true, Non-Fiction=false
+    int intPubDate; // Publication year.
     int intPages; // Number of pages in the book.
-    double dblRating; // double number of a rating
-
-    // Create some test books with a static class.
-    {
-        Books book1 = new Books(0439023528, "The Hunger Games", "Suzanne Collins", "Scholastic", 2008, 374, 4.5);
-        Books book2 = new Books(0451524934, "1984", "George Orwell", "Signet Classic", 1949, 328, 4.5);
-        Books book3 = new Books(0393324818, "Moneyball: The Art of Winning an Unfair Game", "Michael Lewis", "W. W. Norton & Company", 2004, 320, 4.5);
-    }
+    BookRating ratingBlob; // BookRating class for rating the books based on criteria specified in the class.
+    String strImageURL; // A goodie for the GUI guys. Should contain an image for displaying in the GUI.
 
     /* Default Constructor */
     Books() {
+        intID = intIDCounter++;
         lngISBN = 0;
         strTitle = "";
         strAuthor = "";
         strPublisher = "";
+        boolFicNonFic = false;
         intPubDate = 0;
         intPages = 0;
-        dblRating = 0.0;
+        ratingBlob = new BookRating();
     }
 
-    /* Full Constructor */
-    Books(long pISBN, String pTitle, String pAuthor, String pPublisher, int pPubDate, int pPages, double pRating) {
+    /* Full Constructor Without Ratings Specified */
+    Books(long pISBN, String pTitle, String pAuthor, String pPublisher, boolean pFicNonFic, int pPubDate, int pPages, String pImageURL) {
+        intID = intIDCounter++;
         lngISBN = pISBN;
         strTitle = pTitle;
         strAuthor = pAuthor;
         strPublisher = pPublisher;
+        boolFicNonFic = pFicNonFic;
         intPubDate = pPubDate;
         intPages = pPages;
-        dblRating = pRating;
+        ratingBlob = new BookRating();
+        strImageURL = pImageURL;
     }
 
     /* Getters and Setters */
-    private long getISBN(){
+
+    public int getIntID() {
+        return intID;
+    }
+
+    public void setIntID(int intID) {
+        this.intID = intID;
+    }
+
+    public long getISBN(){
         return this.lngISBN;
     }
 
-    private void setISBN(long pISBN) {
+    public void setISBN(long pISBN) {
         this.lngISBN = pISBN;
     }
 
-    private String getTitle(){
+    public String getTitle(){
         return this.strTitle;
     }
 
-    private void setTitle(String pTitle){
+    public void setTitle(String pTitle){
         this.strTitle = pTitle;
     }
 
-    private String getAuthor(){
+    public String getAuthor(){
         return this.strAuthor;
     }
 
-    private void setAuthor(String pAuthor){
+    public void setAuthor(String pAuthor){
         this.strAuthor = pAuthor;
     }
 
-    private String getPublisher(){
+    public String getPublisher(){
         return this.strPublisher;
     }
 
-    private void setPublisher(String pPublisher){
+    public void setPublisher(String pPublisher){
         this.strPublisher = pPublisher;
     }
 
-    private int getPubDate(){
+    public boolean getBoolFicNonFic() {
+        return boolFicNonFic;
+    }
+
+    public void setBoolFicNonFic(boolean boolFicNonFic) {
+        this.boolFicNonFic = boolFicNonFic;
+    }
+
+    public int getPubDate(){
         return this.intPubDate;
     }
 
-    private void setPubDate(int pPubDate){
+    public void setPubDate(int pPubDate){
         this.intPubDate = pPubDate;
     }
 
-    private int getPages(){
+    public int getPages(){
         return this.intPages;
     }
 
-    private void setPages(int pPages){
+    public void setPages(int pPages){
         this.intPages = pPages;
     }
 
-    private double getRatings(){
-        return this.dblRating;
+    public BookRating getRatingBlob() {
+        return ratingBlob;
     }
 
-    private void setRatings(double pRating) {
-        this.dblRating = pRating;
+    public void setRatingBlob(BookRating ratingBlob) {
+        this.ratingBlob = ratingBlob;
+    }
+
+    public String getStrImageURL() {
+        return strImageURL;
+    }
+
+    public void setStrImageURL(String strImageURL) {
+        this.strImageURL = strImageURL;
     }
 
     /* Methods */
 
+    // Create some test books with a static main class.
+    public static void main()
+    {
+        System.out.println("\n***** TESTING BOOKS CLASS *****\n");
+
+        Books book1 = new Books(439023528, "The Hunger Games", "Suzanne Collins", "Scholastic", false, 2008, 374, "http://stuff.com");
+        Books book2 = new Books(451524934, "1984", "George Orwell", "Signet Classic", false, 1949, 328, "http://stuff.com");
+        Books book3 = new Books(393324818, "Moneyball: The Art of Winning an Unfair Game", "Michael Lewis", "W. W. Norton & Company", false, 2004, 320, "http://stuff.com");
+        Books book4 = new Books(140275363, "The Iliad", "Homer", "Penguin Classics", true, 1998, 704, "http://stuff.com");
+
+        System.out.println(book1.toString());
+        System.out.println(book2.toString());
+        System.out.println(book3.toString());
+        System.out.println(book4.toString());
+
+        System.out.println("\n***** ADD RATINGS TO BOOK RATINGS CLASS *****\n");
+
+        book1.rateBook(4.4f, 4.5f, 4.9f, 4.7f);
+        book2.rateBook(3.4f, 2.5f, 2.9f, 2.7f);
+        book3.rateBook(2.4f, 1.5f, 3.9f, 3.7f);
+        book4.rateBook(4.8f, 3.5f, 1.9f, 2.7f);
+
+        System.out.println(book1.toString());
+        System.out.println(book2.toString());
+        System.out.println(book3.toString());
+        System.out.println(book4.toString());
+    }
+
     /* toString() will print in the console the book objects properties. */
     public String toString(){
-        return "ISBN: " + lngISBN+ ", Title: " + strTitle + ", Author: " +
-                strAuthor + ", Publisher: " + strPublisher +
-                ", Year Published: " + intPubDate + ", Pages: " +
-                intPages + ", Rating: " + dblRating + "\n";
+        return String.format("ID: %d, ISBN: %d, Title: %s, Author: %s, Publisher: %s, %s, Year Published: %d, Pages: %d," +
+                        " Long: %s, Image URL: %s\n     Rating: %s",
+                intID, lngISBN, strTitle, strAuthor, strPublisher, this.isFicNonFic(this), intPubDate, intPages,
+                this.isBookLong(this), strImageURL, ratingBlob.toString(this));
 
     }
 
@@ -123,5 +181,23 @@ public static class Books {
         return isLong;
     }
 
+    /* isFicNonFic(Books) will determine if the book is fiction or non-fiction
+       and will give a String saying "Fiction" or "Non-Fiction".
+    */
+    public String isFicNonFic(Books pBooks){
+        String ficOrNon;
+        if (pBooks.getBoolFicNonFic()){
+            ficOrNon = "Fiction";
+        }
+        else {
+            ficOrNon = "Non-Fiction";
+        }
+        return ficOrNon;
+    }
 
+    /* rateBook(float, float, float, float) will use the setRatings method in the BookRating class.
+    */
+    public void rateBook(float pRating, float pLongRating, float pFicNonFicRating, float pGenreRating){
+        this.ratingBlob.setRatings(this, pRating, pLongRating, pFicNonFicRating, pGenreRating);
+    }
 }
